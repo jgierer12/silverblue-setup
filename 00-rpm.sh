ba1aeb1181638163a5fdf5944e4266653fcc12da
Jonas Gierer <jonas@gierer.xyz>
2022-09-19 23:33:05 +0200

    Fix rpm font in firefox

--- 00-rpm.sh
+++ 00-rpm.sh
@@ -18,10 +18,16 @@ rpm-ostree override remove firefox-langpacks &> /dev/null || true # F37+
 rpm-ostree override remove firefox &> /dev/null || true
 
 if [ ! -e /etc/yum.repos.d/tailscale.repo ]; then
-    echo "Installing tailscale repo..."
-    sudo curl -s https://pkgs.tailscale.com/stable/fedora/tailscale.repo -o /etc/yum.repos.d/tailscale.repo > /dev/null
-    # Disable repo_gpgcheck, which doesn't work on Silverblue 36+
-    sudo sed -i 's/repo_gpgcheck=1/repo_gpgcheck=0/' /etc/yum.repos.d/tailscale.repo
+  echo "Installing tailscale repo..."
+  sudo curl -s https://pkgs.tailscale.com/stable/fedora/tailscale.repo -o /etc/yum.repos.d/tailscale.repo > /dev/null
+  # Disable repo_gpgcheck, which doesn't work on Silverblue 36+
+  sudo sed -i 's/repo_gpgcheck=1/repo_gpgcheck=0/' /etc/yum.repos.d/tailscale.repo
+fi
+
+if [ ! -e /usr/local/share/fonts ]; then
+  echo "Linking fonts..."
+  # Required for fonts installed with rpm to be picked up by Firefox flatpak
+  sudo ln -s /usr/share/fonts /usr/local/share/fonts
 fi
 
 echo "Installing layered packages..."
